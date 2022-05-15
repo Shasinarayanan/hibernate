0b3119cd97407d194c0ed0138fb7d94bf30d4778
Shasinarayanan <101916816+Shasinarayanan@users.noreply.github.com>
2022-05-15 08:34:43 -0700

    files were added successfully

--- MainTest.java
+++ MainTest.java
@@ -1,54 +1,53 @@
-package com.edubridge.hibernate.valuetype;
-
-import java.util.Date;
-import java.util.Scanner;
+package com.edubridge.onetoonemapping;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-
-
 public class MainTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
-
-			Configuration cfg = new Configuration();
-			System.out.println("2222222222222222222222222222");
-			cfg.configure("Hibernate.cfg.xml");
-			SessionFactory factory = cfg.buildSessionFactory();
-			System.out.println("4444444444444444444444444");
-			// creating session object
-			Session session = factory.openSession();
-			// Session session=new
-			// AnnotationConfiguration().configure().buildSessionFactory().openSession();
-
-			Transaction t = session.beginTransaction();
-			Address address = new Address();
-			address.setCity("Bangalore");
-			address.setState("Tamilnadu");
-			address.setPincode("632602");
-			address.setStreet("cross street");
-			UserDetails userdetails=new UserDetails();
-			userdetails.setPhone("746573624572");
-			userdetails.setUserId(1);
-			userdetails.setUserName("sasirekha");
-			userdetails.setdob(new Date());
-			userdetails.setAddress(address);
-			session.save(userdetails);
-			t.commit();
-			
-		    session.close();
-		System.out.println("successfully saved");
-	}catch(
-
-	Exception ex)
+			 Configuration cfg=new Configuration();
+			    System.out.println("2222222222222222222222222222");
+			    cfg.configure("hibernate.cfg.xml");//populates the data of the configuration file  
+			    System.out.println("3333333333333333333333333333");
+			    //creating seession factory object  
+			    SessionFactory factory=cfg.buildSessionFactory();  
+			    System.out.println("4444444444444444444444444");
+			    //creating session object  
+			    Session session=factory.openSession();  
+			    System.out.println("5555555555555555555555555");
+			    //creating transaction object  
+			    Transaction t=session.beginTransaction();  
+			    
+			    Address address=new Address();
+			  //  address.setStreet("2nd block");
+			    address.setCity("mangalore");
+			    address.setState("Karnataka");
+			  //  address.setPincode("98653");
+		
+			    
+			    Employee employee=new Employee();
+			    employee.setName("satish");
+			    employee.setEmail("satish@gmail.com");
+			   // Address address1=session.get(Address.class,1);
+			    employee.setAddress(address);//employee is mapping with address
+			    address.setEmployee(employee);//address is mapping with employee
+			    
+			    session.save(employee);
+			    t.commit();
+			    session.close();
+	}catch(Exception ex)
 	{
-		System.out.println("exception " + ex.getMessage());
+		System.out.println(ex.getMessage());
+	}
+		}
+
+	
+
 	}
 
-}
-}
+
